d3eb760f6b7c5dcaf3e94b663082a108a1615be1
Lucas Gonçalves <lucasg@emphasisconf.com.br>
2023-07-28 15:44:24 -0300

    iniciando

---
+++ aula1/main.py
@@ -0,0 +1,26 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'custumers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weigth REAL'
+    ')'
+)
+
+connection.commit()
+
+
+cursor.close()
+connection.close()
